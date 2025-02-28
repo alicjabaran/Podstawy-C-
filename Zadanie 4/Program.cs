dc062ef488c5bbe23d512984e79b76ce67a92154
Alicja Baran <alicja.baran0203@gmail.com>
2025-02-28 13:05:04 +0100

    koniec zajęć

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący boki prostokąta i wypisujący na ekranie jego pole i obwód
+
+            int a;
+            int b; 
+            Console.Write("Podaj a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj b=");
+            b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pole={0}", a * b);
+            Console.WriteLine("Obwód={0}", 2*a + 2*b);
+            Console.ReadKey(true);
+    
+        }
+    }
+}
---
+++ Zadanie 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby całkowite i wypisujący na ekranie iloraz tych liczb z
+            //uwzględnieniem części całkowitej i reszty z dzielenia.
+
+            double a, b;
+            Console.Write("Podaj a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wynik wynosi {0}/{1}={2} a reszta {3}", a, b, a / b, a % b);
+
+            Console.ReadKey(true);
+        }
+    }
+}
