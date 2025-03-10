11a04fe7c8f65bdb07fb269cb1f11f5882d94979
Alicja Baran <a.baran.489@studms.ug.edu.pl>
2025-03-10 16:21:40 +0100

    koniec

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący rok i wypisujący na ekranie informację czy jest to rok przestępny
+            // czy też nie.
+
+            int rok;
+            Console.WriteLine("Podaj rok: ");
+            rok = Convert.ToInt32(Console.ReadLine());
+            if ((rok % 4 == 0) && (rok % 100 != 0)) || (rok % 400 == 0));
+            Console.WriteLine("Rok {0} jest przestępny.", rok);
+            else
+                Console.WriteLine("Rok {0} nie jest przystępny");
+            Console.ReadKey(true);
+
+        }
+    }
+}
