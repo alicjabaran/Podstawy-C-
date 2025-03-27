c85af275c78a1830e9071448e50bb4605753be5d
Alicja <alicja.baran0203@gmail.com>
2025-03-27 19:06:54 +0100

    koniec

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5. Napisać program wczytujący od użytkownika liczbę rzeczywistą x. Wczytuj tak długo wartość
+            //zmiennej x, aż przyjmie ona wartość dodatnią. Wczytaną dodatnią wartość liczby x wypisz na
+            //ekranie.Użyj pętli do -while
+
+            double x;
+            do
+            {
+                Console.WriteLine("Podaj x = ");
+                x = Convert.ToDouble(Console.ReadLine());
+            }
+            while (x <= 0);
+
+            Console.WriteLine("Wczytano liczbę x = {0}", x);
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+
+            int pole;
+            double a, b;
+            do
+            {
+                Console.WriteLine("Wybierz liczbę. Którego pola chcesz użyć?");
+                Console.WriteLine(" 1 - pole prostokąta");
+                Console.WriteLine(" 2 - pole kwadratu");
+                Console.WriteLine(" 3 - pole trójkąta");
+                pole = Convert.ToInt32(Console.ReadLine());
+                switch (pole)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Console.WriteLine("Podaj dlugość boku a = ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Podaj dlugość boku b = ");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Pole prostokąta wynosi = {0}", a * b);
+                        break;
+                    case 2:
+                        Console.WriteLine("Podaj dlugość boku a = ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Pole kwadratu wynosi = {0}", a * a);
+                        break;
+                    case 3:
+                        Console.WriteLine("Podaj dlugość boku a = ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Podaj dlugość boku h = ");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Pole prostokąta wynosi = {0}", a * b / 2.0);
+                        break;
+                    default:
+                        Console.WriteLine("Bląd");
+                        break;
+
+                }
+            } while (pole != 0);
+        }
+    }
+}
+ 
