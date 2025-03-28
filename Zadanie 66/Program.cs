e3efa9335b984aaac4590c2f9cc8289fedcbd7a1
Alicja <alicja.baran0203@gmail.com>
2025-03-28 12:56:01 +0100

    koniec

---
+++ Zadanie 66/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zadeklaruj tablicę zawierającą nazwy dni tygodnia. Wypisz elementy tablicy za pomocą pętli
+            //foreach.
+
+            string[] dni = new string[]
+            {
+                "Poniedzialek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"
+            };
+
+            foreach (string elem in dni)
+                Console.Write("{0}, ", elem);
+
+            Console.ReadKey(true); 
+        }
+    }
+}
