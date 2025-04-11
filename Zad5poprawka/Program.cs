794f876958543a5b553ed4d99c188250845eff58
Alicja <alicja.baran0203@gmail.com>
2025-04-11 12:02:17 +0200

    koniec

---
+++ Zad5poprawka/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad5poprawka
+{
+    internal class Program
+    {
+
+        /*Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb. Przetestuj działanie
+utworzonej funkcji.*/ 
+
+        static int czy_pierwsza(int liczba)
+
+        {
+            int i;
+            for (i = 2; i < liczba / 2; i++)
+                if (liczba % i == 0) return 0;
+            return 1;
+                    
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Czy 5 jest liczbą pierwszą?: {0}", czy_pierwsza(5));
+            Console.WriteLine("Czy 10 jest liczbą pierwszą?: {0}", czy_pierwsza(10));
+            Console.WriteLine("Czy 13 jest liczbą pierwszą?: {0}", czy_pierwsza(13));
+            
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zad5poprawka/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zad5poprawka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zad5poprawka")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("813e1029-07f3-47c8-9f59-6968cd706f21")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
