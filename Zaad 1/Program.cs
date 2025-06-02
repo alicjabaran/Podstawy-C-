cc5474ebdda3e9b51ffc14fd08c4c9b448b188ac
Alicja <alicja.baran0203@gmail.com>
2025-06-02 22:48:20 +0200

    koniec

---
+++ Zaad 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaad_1
+{
+    internal class Program
+    {
+        static long silnia (int n)
+
+        {
+            if (n == 0) return 1;
+            else return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            /*Napisz funkcję , która oblicza wartość silni z zadanej przez argument wywołania funkcji
+wartości liczby naturalnej. Utwórz wersję rekurencyjną tej f*/
+
+            Console.Write("Podaj n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Blędny argument");
+            else Console.WriteLine("{0}!={1}", n, silnia(n));
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
